764b0b16301aac2b7a748a69d3bdba5185bed2c8
Arnold Chis <119848325+Mineix@users.noreply.github.com>
2022-12-13 11:31:08 +0100

    Update kerem user add

--- Verwaltung_Projekt.sh
+++ Verwaltung_Projekt.sh
@@ -8,6 +8,29 @@ source "funktionen.sh"
 
 
 
+
+
+function deletegroup(){
+	fi
+	sleep 2
+}
+
+
+function useradd
+kdialog --inputbox username=echo$(" Wer wird Hinzugefügt")
+
+    echo "Wohin soll der User gespeichert werden"
+    read -p "Eingabe : WHERE2ADD
+    echo "Sind sie Sicher das sie den User hinzufügen wollen"
+function yesno-dialog(){
+        YESNO=0
+        until [ $YESNO = 1 ]
+        do
+		$1
+		read -p "| (0: nein | 1: ja) " YESNO
+        done
+}
+
 function deletegroup(){
 	yesno-dialog where2Find 
 	for I in $(find ${WHERE2FIND} -maxdepth 1 \( -name "*tgz" -o -name "*xz" -o -name "*bzip2" \) )
